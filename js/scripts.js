fc6368ba8d0116c97150a904df0600e522679674
Palse,Swapnil <swapnil.palse@gmail.com>
2019-11-26 00:06:11 -0600

    updated the styles and favicons

--- js/scripts.js
+++ js/scripts.js
@@ -93,7 +93,7 @@ $(document).ready(function () {
                 $('section.navigation').addClass('fixed');
                 $('header').css({
                     "border-bottom": "none",
-                    "padding": "35px 0"
+                    "padding": "17px 0"
                 });
                 $('header .member-actions').css({
                     "top": "26px",
@@ -105,7 +105,7 @@ $(document).ready(function () {
                 $('section.navigation').removeClass('fixed');
                 $('header').css({
                     "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
-                    "padding": "50px 0"
+                    "padding": "35px 0"
                 });
                 $('header .member-actions').css({
                     "top": "41px",
